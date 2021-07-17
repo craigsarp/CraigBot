30981fc6967e27d518e7d1e19e27e653c69688fe
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 00:40:26 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -6,25 +6,32 @@ module.exports = {
         if (!message.member.hasPermission("ADMINISTRATOR")) {
             return message.channel.send("No no no.");
         }
+        const PrefixSchema = require('../schema/PrefixSchema')
+        const newprefix = args[0]
+    if(!newprefix) return message.channel.send("please provide a new prefix!") 
+    if(newprefix.length > 5) return message.channel.send('This prefix is too long, you have max 5 caracters')
 
-        if (!args[0]) {
-            return message.channel.send("Please give the prefix you want to set");
+
+    let data;
+    try {
+        data = await PrefixSchema.findOne({
+            _id: message.guild.id
+        })
+        if(!data) {
+            let newdata = await PrefixSchema.create({
+                _id: message.guild.id,
+                newPrefix: newprefix
+            })
+            newdata.save()
+        } else {
+            await PrefixSchema.findOneAndUpdate({
+                _id: message.guild.id,
+                newPrefix: newprefix,
+            })
         }
-
-        if (args[1]) {
-            return message.channel.send("You can not send prefix a double argument");
-        }
-
-        if (args[0].length > 3) {
-            return message.channel.send("You can not send prefix more than 3 characters");
-        }
-
-        if (args.join("") === '?') {
-            db.delete(`prefix_${message.guild.id}`);
-            return message.channel.send("Reseted Prefix ✅");
-        }
-
-        db.set(`prefix_${message.guild.id}`, args[0])
-        message.channel.send(`Bot Prefix Set To ${args[0]}`);
+        message.channel.send(`The Prefix has Been set to ${newprefix}`)
+    } catch (err) {
+        console.log(err)
+    }
     }
 }
