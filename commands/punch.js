2451c32222e7d5cb440ce8684aae7ff3e226dfe7
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 00:11:23 +0100

    Update punch.js

--- commands/punch.js
+++ commands/punch.js
@@ -9,7 +9,7 @@ module.exports = {
 
 
             let embed = new Discord.MessageEmbed()
-                .setTitle(`You just punched: @` + message.mentions.users.first().tag + `!`)
+                .setTitle(`You just punched your enemy: @` + message.mentions.users.first().tag + `!`)
                 .setImage(`https://c.tenor.com/p3Hgg8D0mFMAAAAM/anime-punch.gif`)
                 .setColor("RANDOM")
             message.channel.send(embed)
