17cd9b53c93297ed3a20811b5a0ef9606e11e19b
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-17 23:56:21 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'say',
   description: 'Say Command',
   usage: 'Used to make the bot say something.',
-  execute(message, args, Discord, db) {
+  execute(message, args, Discord, db, config) {
         const SayMessage = message.content.slice(4).trim();
         let prefix = db.get(`prefix_${message.guild.id}`)
         if (prefix === null) prefix = config.default_prefix;
