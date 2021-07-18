938ddb10c508e21c5606ebe8df8f625b84f32684
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 14:28:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,33 @@ const fs = require('fs');
 
 const ms = require('ms');
 
-const mongoose = require('./utils/mongoose');
-mongoose.init();
+const mongoose = require('mongoose');
+const dbOptions = {
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+           autoIndex: false,
+           reconnectTries: Number.MAX_VALUE,
+           reconnectInterval: 500,
+           poolSize: 5,
+           connectTimeoutMS: 10000,
+           family:4
+       };
+
+       mongoose.connect(process.env.mongoPath, dbOptions);
+       mongoose.set('useFindAndModify', false);
+       mongoose.Promise = global.Promise;
+
+       mongoose.connection.on('connected', () => {
+           console.log('Mongoose has successfully connected!');
+       });
+
+       mongoose.connection.on('err', err => {
+           console.error(`Mongoose connection error: \n${err.stack}`);
+       });
+
+       mongoose.connection.on('disconnected', () => {
+           console.warn('Mongoose connection lost');
+       });
 
 const cooldown = new Set();
 
