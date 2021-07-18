169592e9d91db4861a105e612e5b9a325c02f8e7
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 22:36:43 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'help',
     description: 'Help Command',
     usage: 'Used to help the dumb.',
-    execute(message, args, Discord, fs, config) {
+    run: async(message, args, Discord, fs, config) => {
         let prefix;
         const data = await prefixSchema.findOne({
             GuildID: message.guild.id
