29c41443307771a7dda6b48ff28fe147f6694e68
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 15:06:50 +0100

    Update prefix-reset.js

--- commands/prefix-reset.js
+++ commands/prefix-reset.js
@@ -3,18 +3,7 @@ module.exports = {
     description: 'Prefix Reset Command',
     usage: 'Used to reset the prefix on your server.',
     run : async(client, message, args, prefixSchema) => {
-        message.channel.send("Are you sure you want to reset the prefix?").then(async (msg) => {
-            const emoji = await confirmation(msg, message.author, ['✅', '❌'], 10000)
-            if(emoji === '✅') {
-                msg.delete()
-                await prefixSchema.findOneAndDelete({ Guild : message.guild.id })
-                message.channel.send(`The prefix has been reset to ${prefix}`)
-            }
-            if(emoji === '❌') {
-                msg.delete()
-                message.channel.send('reset prefix has been cancelled.')
-            }
-        })
-
+               await prefixSchema.findOneAndDelete({ Guild : message.guild.id })
+               message.channel.send(`The prefix has been reset to ?`)
     }
 }
