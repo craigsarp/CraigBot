6404a61888f92505121651cb5bbdc0a764ef451b
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 13:46:42 +0100

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -3,22 +3,16 @@ module.exports = {
     description: 'Balance Command',
     usage: 'Used to check how much money you have.',
     execute(message, args, db, Discord, config) {
-        let prefix = db.get(`prefix_${message.guild.id}`)
-        if (prefix === null) prefix = config.default_prefix;
-        if (db.get(`user_${message.author.id}.bal`) === null) {
-            message.reply(`The user needs to use \`${prefix}start\``)
-        } else {
-            let user = message.author.id;
-            let bal = db.get(`user_${message.author.id}.bal`)
-
-            const embed = new Discord.MessageEmbed()
-                .setTitle(`${message.author.username}\'s Balance`)
-                .setDescription(`${bal} :coin:`)
-                .setColor("RANDOM")
-                .setTimestamp()
-
-            message.channel.send(embed)
-
-        }
+        let member = message.mentions.users.first() || message.author;
+ 
+        let user = await mongoEconomy.find(member.id, message.guild.id);
+ 
+        message.channel.send({
+            embed: {
+                color: `RANDOM`,
+             title: `${member.tag}'s Balance`,
+             description: `Money: ${user.money}\nBank: ${user.bank}\nTotal: ${user.money + user.bank}`
+          }
+        })
     }
 }
