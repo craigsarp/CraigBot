f7d328d5c7d3307b6ca08eb27bdd2cfa150b217e
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 14:53:58 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -2,29 +2,27 @@ module.exports = {
     name: 'prefix',
     description: 'Prefix Command',
     usage: 'Used to change the prefix on your server.',
-    execute(message, args, Discord, db) {
-        if (!message.member.hasPermission("ADMINISTRATOR")) {
-            return message.channel.send("No no no.");
-        }
-
-        if (!args[0]) {
-            return message.channel.send("Please give the prefix you want to set");
-        }
-
-        if (args[1]) {
-            return message.channel.send("You can not send prefix a double argument");
-        }
-
-        if (args[0].length > 3) {
-            return message.channel.send("You can not send prefix more than 3 characters");
-        }
-
-        if (args.join("") === '?') {
-            db.delete(`prefix_${message.guild.id}`);
-            return message.channel.send("Reseted Prefix ✅");
-        }
-
-        db.set(`prefix_${message.guild.id}`, args[0])
-        message.channel.send(`Bot Prefix Set To ${args[0]}`);
+    run : async(client, message, args) => {
+        const res = await args.join(" ")
+        if(!res) return message.channel.send('Please specify a prefix to change to.')
+        prefixSchema.findOne({ Guild : message.guild.id }, async(err, data) => {
+            if(err) throw err;
+            if(data) {
+                prefixSchema.findOneAndDelete({ Guild : message.guild.id })
+                data = new prefixSchema({
+                    Guild : message.guild.id,
+                    Prefix : res
+                })
+                data.save()
+                message.channel.send(`Your prefix has been updated to **${res}**`)
+            } else {
+                data = new prefixSchema({
+                    Guild : message.guild.id,
+                    Prefix : res
+                })
+                data.save()
+                message.channel.send(`Custom prefix in this server is now set to **${res}**`)
+            }
+        })
     }
 }
