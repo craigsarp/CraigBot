afbee128925917f753b5c75943da5760b5aa359a
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 16:20:12 +0100

    Delete prefix-reset.js

--- commands/prefix-reset.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'prefix-reset',
-    description: 'Prefix Reset Command',
-    usage: 'Used to reset the prefix on your server.',
-    run : async(client, message, args, prefixSchema) => {
-               await prefixSchema.findOneAndDelete({ Guild : message.guild.id })
-               message.channel.send(`The prefix has been reset to ?`)
-    }
-}
