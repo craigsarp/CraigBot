c897ac5e3a507976ba769d185180ad8b45e5b87a
craigsarp <85460123+craigsarp@users.noreply.github.com>
2021-07-18 16:45:32 +0100

    Update prefix.js

--- models/prefix.js
+++ models/prefix.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = new mongoose.Schema({
     Prefix: String,
-    GuildID: String
+    guildID: String
 });
 
 const MessageModel = module.exports = mongoose.model('prefixes', Schema);
